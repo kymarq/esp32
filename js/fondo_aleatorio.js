7c6c060cd8955c69d3c57faaac92e8e1038d6aab
kymarq <feykrame@gmail.com>
2025-05-02 21:38:59 +0200

    Add files via upload

---
+++ js/fondo_aleatorio.js
@@ -0,0 +1,3 @@
+var colors = ['#93B5C6', '#DDEDAA', '#F0CF65'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.getElementById('fondo').style.backgroundColor = random_color;
